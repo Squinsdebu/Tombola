75d5721d25db50507db0cbef400a7b691be8c46d
Gabriele Bracci <83134413+Squinsdebu@users.noreply.github.com>
2023-01-30 15:06:41 +0100

    seconda prova

--- app/src/main/java/com/example/tombola/Gioco.java
+++ app/src/main/java/com/example/tombola/Gioco.java
@@ -1,14 +1,43 @@
 package com.example.tombola;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Gioco extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gioco);
+        setContentView(R.layout.activity_prima_pagina);
+
+
+        final EditText NomeGiocatoreEditText = findViewById(R.id.NomeGiocatoreEditText);
+        final AppCompatButton BottoneStart = findViewById(R.id.BottoneStart);
+
+        BottoneStart.setOnClickListener(view -> {
+            final String getPlayerName = NomeGiocatoreEditText.getText().toString();
+
+            if(getPlayerName.isEmpty()){
+                Toast.makeText(Gioco.this, "Inserisci il tuo nickname", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                //creo L'Intent che apre la main activity
+                Intent intent = new Intent (Gioco.this, MainActivity.class);
+
+                //aggiungo il nome utente all'Intent
+                intent.putExtra("playername", getPlayerName);
+
+                //apro la main
+                startActivity(intent);
+
+                //destroy this activity (PlayerName)
+                finish();
+            }
+        });
     }
 }
