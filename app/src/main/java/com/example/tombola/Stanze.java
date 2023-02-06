7d18537fe8eb034c84a7d2b4bea8b7b22e3123eb
FGiacomo <giacomo.fantato@gmail.com>
2023-02-06 16:27:41 +0100

    V1.0.

--- app/src/main/java/com/example/tombola/Stanze.java
+++ app/src/main/java/com/example/tombola/Stanze.java
@@ -70,6 +70,7 @@ public class Stanze extends AppCompatActivity {
             }
         });
 
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
